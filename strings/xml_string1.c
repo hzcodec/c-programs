94886edaee1778d0abbbdf909385f3703b06ec73
Heinz Samuelsson <heinz@raspiClear.lan>
2015-09-19 19:01:45 +0200

    strings added.

---
+++ strings/xml_string1.c
@@ -0,0 +1,70 @@
+/*
+    Auther      : Heinz Samuelsson
+    Date        : 2015-09-19
+    File        : xml_string1.c
+    Reference   : -
+    Description : Build an XML-string and print it out.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+const char EHSL_PRODTEST_PORT_TEST_RESPONSE_HEAD[] = "\
+<?xml version=\"1.0\"?>\n\
+<methodResponse>\n\
+  <params>\n\
+    <param>\n\
+      <value>\n\
+        <struct>\n\
+          <member>\n\
+            <name>Result</name>\n\
+            <value>\n\
+              <array>\n";
+
+const char EHSL_PRODTEST_PORT_TEST_CASE_RESULT[] = "\
+                <data>\n\
+                  <value>\n\
+                    <struct>\n\
+                      <member>\n\
+                        <name>PortName</name>\n\
+                        <value>\n\
+                          <string>%s</string>\n\
+                        </value>\n\
+                      </member>\n\
+                    </struct>\n\
+                  </value>\n\
+                </data>\n";
+
+const char EHSL_PRODTEST_PORT_TEST_RESPONSE_TAIL[] = "\
+              </array>\n\
+            </value>\n\
+          </member>\n\
+        </struct>\n\
+      </value>\n\
+    </param>\n\
+  </params>\n\
+</methodResponse>\n";
+
+
+int main()
+{
+    char stringA[10000];
+
+    printf("=============================================================\n");
+
+    // build xml string
+    sprintf(stringA, EHSL_PRODTEST_PORT_TEST_RESPONSE_HEAD);
+    printf("%s", stringA);
+
+    sprintf(stringA, EHSL_PRODTEST_PORT_TEST_CASE_RESULT, "Port A");
+    printf("%s", stringA);
+
+    sprintf(stringA, EHSL_PRODTEST_PORT_TEST_RESPONSE_TAIL);
+    printf("%s\n", stringA);
+
+    printf("=============================================================\n");
+
+    return 0;
+}
+
