39de1e3d716490edcbe65988d9b8ea84806bba42
heinz.samuelsson <heinz.samuelsson@unjo.com>
2016-08-05 15:09:34 +0200

    function_pointers dir added

---
+++ function_pointers/function_pointer1.c
@@ -0,0 +1,40 @@
+/*
+    Auther       : Heinz Samuelsson
+    Date         : 2016-08-05
+    File         : funct_ptr.c
+    Reference    : http://www.gammon.com.au/callbacks
+    Description  : Example of function pointer.
+*/ 
+ 
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef void (*GenericMessageFunction)();
+
+
+void sayHello ()
+  {
+    printf("Hello\n");  
+  }
+
+void sayGoodbye ()
+  {
+    printf("Goodbye\n");  
+  }
+
+
+void checkPin (const int pin, GenericMessageFunction response)
+{
+  printf("pin= %d\n", pin);
+  response();
+}
+
+
+int main()
+{
+  checkPin(8, sayHello);    
+  checkPin(9, sayGoodbye);
+
+  return 0;
+}
