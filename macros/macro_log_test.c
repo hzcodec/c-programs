1afbe6a9c44b611438887d91d8784fd1811ec95f
heinz.samuelsson <heinz.samuelsson@unjo.com>
2016-11-11 11:20:46 +0100

    Code cleanup of macro_log_test

--- macros/macro_log_test.c
+++ macros/macro_log_test.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// (condition) ? (do something) : (do something else)
-// if _value less than 0 then *2 else *10
 #define LOG printf("Line: %d in file '%s' (function: %s)\n", \
                     __LINE__, __FILE__, __func__) 
 
