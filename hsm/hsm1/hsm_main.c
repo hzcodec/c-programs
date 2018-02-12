27b38268ab12dd6a8325ad9f6af2b1504f321b93
Heinz Samuelsson <heinz.samuelsson@gmail.com>
2018-02-12 18:28:20 +0100

    Mupp

--- hsm/hsm1/hsm_main.c
+++ hsm/hsm1/hsm_main.c
@@ -87,18 +87,6 @@ static const struct state *main_sm_impl(struct statemachine *sm, const struct ev
 	/* Following 3 events for EMCY check during powering up */
 	/* Special trick to use DISABLED as output during start up to be able to read status on EMCY button */
 	case EV_ENTRY: {
-		/* PP outputs */
-		// HzS - GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-		// HzS - GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-		// HzS - GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-		// HzS - GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-		// HzS - GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;	// DISABLED_A_N
-		// HzS - GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-		// HzS - dio_set_output(DOUT_DISABLED_A_N, true);
-		// HzS - dio_set_output(DOUT_EMERG_EN, true);
-
-		// HzS -  timer_set(&delay, 50);
 		check_emcy_pin_manually = true;
 
 		statemachine_subscribe_do(sm);
@@ -110,13 +98,6 @@ static const struct state *main_sm_impl(struct statemachine *sm, const struct ev
 		return statemachine_event_handled();
 	}
 	case EV_DO: {
-		// HzS -  if (timer_expired(&delay) && check_emcy_pin_manually)			// only check pins manually until "powered" state is entered,
-		// HzS -  {																// then the selftest and BRK function will take over
-			// HzS - if( !dio_get_input(DIN_EMERG_BREAK_A_N_PB5) && !dio_get_input(DIN_EMERG_BREAK_A_N_PD2) )
-			// HzS - {
-			// HzS - 	rgb_led_color(RGB_RED);
-			// HzS - }
-		// HzS -  }
 		break;
 	}
 	}
@@ -165,7 +146,6 @@ static const struct state *wait_for_button_impl(struct statemachine *sm, const s
 	struct main_sm *m = container_of(sm, struct main_sm, sm);
 	switch (ev->id) {
 	case EV_ENTRY: {
-		// HzS -  timer_set(&m->t_main, 2000);
 		statemachine_subscribe_do(sm);
 		return statemachine_event_handled();
 	}
@@ -183,7 +163,6 @@ static const struct state *wait_for_button_impl(struct statemachine *sm, const s
 		return &shutdown;
 	}
 	case EV_DO: {
-		// HzS -  if (timer_expired(&m->t_main)) {
 		if (1 == 0){
 			return &shutdown;
 		}
@@ -197,8 +176,6 @@ static const struct state *wait_for_power_impl(struct statemachine *sm, const st
 {
 	switch (ev->id) {
 	case EV_ENTRY: {
-
-		// HzS -  rgb_led_color(RGB_GREEN_FLASH(100));
 
 		hsm_broadcast(sm, EV_CAN_START, 0);
 		return statemachine_event_handled();
@@ -226,14 +203,6 @@ static const struct state *check_supply_impl(struct statemachine *sm, const stru
 		return statemachine_event_handled();
 	}
 	case EV_DO: {
-		 // HzS - if (timer_expired(&m->t_main)) {
-		// HzS - 	if ((s16b16)aio_get_input(AIN_VBAT) * 75 > int_s16b16(30)) {
-		// HzS - 		return &powered;
-		// HzS - 	} else {
-		// HzS - 		// HzS -  log_event(LOG_EV_POWER_OFF_SUPPLY);
-		// HzS - 		return &error;
-		// HzS - 	}
-		// HzS - }
 		break;
 	}
 	}
@@ -267,22 +236,16 @@ static const struct state *bt_pair_impl(struct statemachine *sm, const struct ev
 	switch (ev->id) {
 	case EV_ENTRY: {
 		statemachine_subscribe_do(sm);
-		// HzS -  timer_set(&s->t_main, 30000);
-		// HzS -  bt_enable_pairing(true);
-		// HzS -  rgb_led_color(RGB_BLUE_FLASH(100));
 		return statemachine_event_handled();
 	}
 	case EV_EXIT: {
 		statemachine_unsubscribe_do(sm);
-		// HzS -  bt_enable_pairing(false);
-		// HzS -  rgb_led_color(RGB_OFF);
 		return statemachine_event_handled();
 	}
 	case EV_BT_CONNECT: {
 		return &idle;
 	}
 	case EV_DO: {
-		// HzS -  if (timer_expired(&s->t_main)) {
 		if (1 == 0) {
 			return &idle;
 		}
@@ -314,7 +277,6 @@ static const struct state *enter_boot_impl(struct statemachine *sm, const struct
 		return statemachine_event_handled();
 	}
 	case EV_CAN_UNPOWERED: {
-		// HzS -  boot_start_bootload(BOOT_ID_AREA_BOOT2);
 		return &error;
 	}
 	}
@@ -334,8 +296,6 @@ static const struct state *wait_save_impl(struct statemachine *sm, const struct 
 	switch (ev->id) {
 	case EV_ENTRY: {
 		save_done_flag = false;
-		// HzS -  timer_set(&e2_write_timeout_timer, 20000);
-		// HzS -  log_save(save_done);
 		statemachine_subscribe_do(sm);
 		return statemachine_event_handled();
 	}
@@ -367,18 +327,7 @@ static const struct state *powered_impl(struct statemachine *sm, const struct ev
 		/* Following for EMCY check during powering up */
 		/* Restore DOUT_DISABLED_A_N to input and clear variable for manually checking status of EMCY button status */
 		check_emcy_pin_manually = false;
-		// HzS -  dio_set_output(DOUT_DISABLED_A_N, false);
-		/* Inputs w/ pull-down */
-		// HzS - GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-		// HzS - GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-		// HzS - GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
-		// HzS - GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-		// HzS - GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;	// DISABLED_A_N
-		// HzS - GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-
-		// HzS -  dio_set_output(DOUT_POWER_AUX_EN, true);
-		// HzS -  log_event(LOG_EV_POWER_ON);
+		/ HzS -  log_event(LOG_EV_POWER_ON);
 		statemachine_subscribe_do(sm);
 		return statemachine_event_handled();
 	}
@@ -431,35 +380,11 @@ static const struct state *bist_step_1_impl(struct statemachine *sm, const struc
 	struct main_sm *m = container_of(sm, struct main_sm, sm);
 	switch (ev->id) {
 	case EV_ENTRY: {
-		// HzS -  timer_set(&m->t_bist, 20);
-		/* Prepare test */
-		// HzS - dio_set_output(DOUT_EMERG_EN, false);
-		// HzS - dio_set_output(DOUT_DRIVE_A_EN, false);
-		// HzS - dio_set_output(DOUT_DRIVE_B_EN, false);
 		return statemachine_event_handled();
 	}
 	case EV_DO: {
 		// HzS -  if (timer_expired(&m->t_bist)) {
 			uint32_t major = 0;
-			// HzS -  boot_get_int(BOOT_ID_AREA_BOOT, 7, &major);
-			// HzS - if (major >= 2
-			// HzS -  && dio_get_input(DIN_EMERG_BREAK_A_N_PB5) == 0
-			// HzS -  && dio_get_input(DIN_EMERG_BREAK_A_N_PD2) == 0
-			// HzS -  && dio_get_input(DIN_EMERG_BREAK_B_N_PA10) == 0
-			// HzS -  && dio_get_input(DIN_EMERG_BREAK_B_N_PE15) == 0
-			// HzS -  && dio_get_input(DIN_DISABLED_A_N) == 0
-			// HzS -  && dio_get_input(DIN_DISABLED_B_N) == 0) {
-			// HzS - 	return &bist_step_2; /* Next step or active */
-			// HzS - } else if (major == 1
-			// HzS -  && dio_get_input(DIN_EMERG_BREAK_A_N_PB5) == 0
-			// HzS -  && dio_get_input(DIN_EMERG_BREAK_A_N_PD2) == 0
-			// HzS -  && dio_get_input(DIN_EMERG_BREAK_B_N_PE15) == 0) {
-			// HzS - 	return &bist_step_2_old; /* Next step or active */
-			// HzS - } else {
-			// HzS - 	log_event(LOG_EV_SELF_TEST_FAIL_BREAK);
-			// HzS - 	return &error;
-			// HzS - }
-		// HzS -  }
 		break;
 	}
 	}
@@ -471,22 +396,9 @@ static const struct state *bist_step_2_old_impl(struct statemachine *sm, const s
 	struct main_sm *m = container_of(sm, struct main_sm, sm);
 	switch (ev->id) {
 	case EV_ENTRY: {
-		// HzS -  timer_set(&m->t_bist, 50);
-		/* Prepare test */
-		// HzS -  dio_set_output(DOUT_EMERG_EN, true);
 		return statemachine_event_handled();
 	}
 	case EV_DO: {
-		// HzS -  if (timer_expired(&m->t_bist)) {
-			// HzS - if (dio_get_input(DIN_EMERG_BREAK_A_N_PB5) != 0
-			// HzS -  && dio_get_input(DIN_EMERG_BREAK_A_N_PD2) != 0
-			// HzS -  && dio_get_input(DIN_EMERG_BREAK_B_N_PE15) != 0) {
-			// HzS - 	return &bist_step_3; /* Next step or active */
-			// HzS - } else {
-			// HzS - 	log_event(LOG_EV_SELF_TEST_FAIL_EMSTOP);
-			// HzS - 	return &error;
-			// HzS - }
-		// HzS -  }
 		break;
 	}
 	}
@@ -498,23 +410,9 @@ static const struct state *bist_step_2_impl(struct statemachine *sm, const struc
 	struct main_sm *m = container_of(sm, struct main_sm, sm);
 	switch (ev->id) {
 	case EV_ENTRY: {
-		// HzS -  timer_set(&m->t_bist, 50);
-		/* Prepare test */
-		// HzS -  dio_set_output(DOUT_EMERG_EN, true);
-		// HzS -  dio_set_output(DOUT_DRIVE_A_EN, true);
 		return statemachine_event_handled();
 	}
 	case EV_DO: {
-		// HzS -  if (timer_expired(&m->t_bist)) {
-			// HzS - if (dio_get_input(DIN_EMERG_BREAK_A_N_PB5) != 0
-			// HzS -  && dio_get_input(DIN_EMERG_BREAK_A_N_PD2) != 0
-			// HzS -  && dio_get_input(DIN_DISABLED_A_N) != 0) {
-			// HzS - 	return param.num_motor_ch > 1 ? & bist_step_2_5 : &bist_step_3; /* Next step or active */
-			// HzS - } else {
-			// HzS - 	log_event(LOG_EV_SELF_TEST_FAIL_EMSTOP);
-			// HzS - 	return &error;
-			// HzS - }
-		// HzS -  }
 		break;
 	}
 	}
@@ -526,23 +424,9 @@ static const struct state *bist_step_2_5_impl(struct statemachine *sm, const str
 	struct main_sm *m = container_of(sm, struct main_sm, sm);
 	switch (ev->id) {
 	case EV_ENTRY: {
-		// HzS -  timer_set(&m->t_bist, 50);
-		/* Prepare test */
-		// HzS -  dio_set_output(DOUT_EMERG_EN, true);
-		// HzS -  dio_set_output(DOUT_DRIVE_B_EN, true);
 		return statemachine_event_handled();
 	}
 	case EV_DO: {
-		// HzS -  if (timer_expired(&m->t_bist)) {
-			// HzS - if (dio_get_input(DIN_EMERG_BREAK_B_N_PE15) != 0
-			// HzS -  && dio_get_input(DIN_EMERG_BREAK_B_N_PA10) != 0
-			// HzS -  && dio_get_input(DIN_DISABLED_B_N) != 0) {
-			// HzS - 	return &bist_step_3; /* Next step or active */
-			// HzS - } else {
-			// HzS - 	log_event(LOG_EV_SELF_TEST_FAIL_EMSTOP);
-			// HzS - 	return &error;
-			// HzS - }
-		// HzS -  }
 		break;
 	}
 	}
@@ -558,10 +442,6 @@ static const struct state *bist_step_3_impl(struct statemachine *sm, const struc
 			/* Check motor setup ok, basically checking current offsets. */
 			// HzS -  if (hw_init()) {
 				return &bist_step_4; /* Next step or active */
-			// HzS - } else {
-			// HzS - 	// HzS -  log_event(LOG_EV_SELF_TEST_FAIL_CURR);
-			// HzS - 	return &error;
-			// HzS - }
 			break;
 		}
 		case EV_EM_STOP:
@@ -591,14 +471,6 @@ static const struct state *bist_step_4_impl(struct statemachine *sm, const struc
 		}
 		case EV_DO:
 		{
-			// HzS -  if (timer_expired(&m->t_bist)) {
-				 // HzS - if (!motor_sincos_has_fault()) {
-				// HzS - 	return &bist_step_5; /* Next step or active */
-				// HzS - } else {
-				// HzS - 	// HzS -  log_event(LOG_EV_SELF_TEST_FAIL_SINCOS);
-				// HzS - 	return &error;
-				// HzS - }
-			// HzS -  }
 			break;
 		}
 		case EV_EM_STOP:
@@ -631,13 +503,6 @@ static const struct state *bist_step_5_impl(struct statemachine *sm, const struc
 				// HzS -  log_event(LOG_EV_SELF_TEST_FAIL_DRIVE_TEMP);
 				return &temp_error;
 			}
-			 // HzS - if (param.num_motor_ch > 1) {
-			// HzS - 	// HzS -  temp = temp_get_value(TEMP_DRIVE_B);
-			// HzS - 	if (temp < int_s16b16(-40) || temp > int_s16b16(100)) {
-			// HzS - 		// HzS -  log_event(LOG_EV_SELF_TEST_FAIL_DRIVE_TEMP);
-			// HzS - 		return &temp_error;
-			// HzS - 	}
-			// HzS - }
 			return &bist_step_6;
 		}
 		case EV_EM_STOP:
@@ -664,8 +529,6 @@ static const struct state *bist_step_6_impl(struct statemachine *sm, const struc
 		}
 		case EV_EXIT:
 		{
-			// HzS -  motor_set_iq(0, 0);
-			// HzS -  motor_set_iq(1, 0);
 			return statemachine_event_handled();
 		}
 		case EV_DO:
