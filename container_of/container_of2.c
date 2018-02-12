5edc3553cc1cd5dd99e7cc57f12c7878f943d3e3
heinz.samuelsson <heinz.samuelsson@unjo.com>
2018-02-12 13:58:02 +0100

    Added container_of2

---
+++ container_of/container_of2.c
@@ -0,0 +1,41 @@
+/*
+    Auther       : Heinz Samuelsson
+    Date         : 2018-02-11
+    File         : container_of2.c
+    Reference    : https://www.sockfd.net/c/get-parent-structure-by-its-member
+    Description  : How to get the parent structure by its member.
+*/ 
+ 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+ 
+#define container_of(ptr, type, member) ({                      \
+        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+        (type *)( (char *)__mptr - offsetof(type,member) );})
+
+
+typedef struct my_struct
+{
+        int data;
+	char name[100];
+	double f;
+} my_struct_t; 
+
+
+int main()
+{
+        // declare a struct t with basic values
+	my_struct_t t = {1, "Hello World", 0.1};
+
+	// Get the parent struct which contains out pointer (ptr).
+	// We give the type as 2nd parameter (my_struct_t) and the
+	// name of the member ('data').
+	my_struct_t *t2 = (my_struct_t *)container_of(&t.data, my_struct_t, data);
+
+	// now t2 is equal to the parent of the member 'data'
+	printf("%s\n", t2->name);
+
+        return 0;
+}
+
