f08290a39c9365e39aee8bea7845bae5749ba05a
heinz.samuelsson <heinz.samuelsson@unjo.com>
2018-11-21 15:33:19 +0100

    makefile1 make updates

--- makefile1/src/main.mk
+++ makefile1/src/main.mk
@@ -1,6 +1,9 @@
-$(info main.mk)
+$(info --- main.mk ---)
 
 TEST_PGM_SOURCES = \
 file1.c \
 file2.c \
 main.c
+
+$(info TEST_PGM_SOURCES="$(TEST_PGM_SOURCES)")
+$(info -----------------------------------------)
