2e5fcc375a83e77954f76c671de84617b32d7ed5
heinz.samuelsson <heinz.samuelsson@unjo.com>
2019-01-03 13:53:44 +0100

    Code cleanup

--- hsm/fsm/fsm1/fsm_test1.c
+++ hsm/fsm/fsm1/fsm_test1.c
@@ -26,9 +26,9 @@ STATE(FSM_Init)
 END_STATE;
 
 STATE(FSM_Successful)
-ON_ENTRY{
-	printf("  Entry for FSM_Successful\n");
-}
+	ON_ENTRY{
+		printf("  Entry for FSM_Successful\n");
+	}
 
 	for (int i=0; i<3; i++) {
 		printf("FSM_Successful [%d]\n", i);
